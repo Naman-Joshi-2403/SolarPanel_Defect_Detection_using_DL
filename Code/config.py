e4462f36ad9ac6f63d445385ca6c6c5bb4e0f977
Naman Joshi <namanjoshi2403@gmail.com>
2025-07-20 14:28:15 +0530

    updated preprocess function

--- Code/config.py
+++ Code/config.py
@@ -2,4 +2,8 @@ import os
 
 base_user_dir = os.path.expanduser('~')
 
-input_folder_path = os.path.join(base_user_dir, r'OneDrive\Desktop\Guvi_Projects\Solar Panel\Faulty_solar_panel')
+input_folder_path = os.path.join(base_user_dir, r'OneDrive\Desktop\Guvi_Projects\Solar Panel\Faulty_solar_panel_raw_data')
+preprocess_image_path = os.path.join(base_user_dir, r'OneDrive\Desktop\Guvi_Projects\Solar Panel\Faulty_solar_panel')
+img_tager_size = (256, 256)
+background_color_padding = (0,0,0)
+image_extension = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff')
--- Code/methords.py
+++ Code/methords.py
@@ -1,17 +1,39 @@
 import pandas as pd 
 import os
 from PIL import Image
-from config import input_folder_path
+from config import input_folder_path, img_tager_size, background_color_padding, preprocess_image_path, image_extension
 
-def data_collection(folder_path):
-    for file in os.listdir(folder_path):
-        for img in os.listdir(os.path.join(folder_path, file)):
-            with Image.open(os.path.join(folder_path, file, img)) as img_file:
-                current_dimension = img_file.size
-                print(current_dimension)
+def data_collection(folder_path, output_folder_path):
+    try:
+        ## Ensure output dir is present
+        os.makedirs(output_folder_path, exist_ok=True)
+        ## Catagories Folder
+        for file in os.listdir(folder_path):
+            ## Ensure output catagory folder present
+            os.makedirs(os.path.join(output_folder_path, file), exist_ok=True)
+            ## Images in folder
+            for img in os.listdir(os.path.join(folder_path, file)):
+                if img.lower().endswith(image_extension): 
+                    with Image.open(os.path.join(folder_path, file, img)) as img_file:
+                        current_dimension = img_file.size
+                        ratio = min(img_tager_size[0] / current_dimension[0], img_tager_size[1] / current_dimension[1])
+                        new_dimension = (int(current_dimension[0] * ratio), int(current_dimension[1] * ratio))
+                        resize_image = img_file.resize(size=new_dimension, resample=Image.LANCZOS)
 
-            print(f"{file} : {img}")
+                        ## Creating new Canvas
+                        padded_image = Image.new('RGB', size=img_tager_size, color=background_color_padding)
 
-    pass
+                        ## calculating center of canvas
+                        x_coordinate = (img_tager_size[0] - new_dimension[0]) // 2 
+                        y_coordinate = (img_tager_size[1] - new_dimension[1]) // 2 
 
-data_collection(input_folder_path)
+                        padded_image.paste(im=resize_image, box=(x_coordinate, y_coordinate))
+
+                        padded_image.save(os.path.join(output_folder_path, file, img))
+
+                        print(f'Successfully saved image to path {os.path.join(output_folder_path, file, img)}')
+
+    except Exception as e:
+        print(f"Error Occured while pre processiing the images. Error: {e}")
+
+data_collection(input_folder_path, preprocess_image_path)
