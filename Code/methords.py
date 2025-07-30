f5dd171ec2700e786944f46d029156a7ec2f7b0f
Naman Joshi <namanjoshi2403@gmail.com>
2025-07-30 21:56:20 +0530

    update methords

--- Code/methords.py
+++ Code/methords.py
@@ -15,19 +15,19 @@ def data_collection(folder_path, output_folder_path):
             os.makedirs(os.path.join(output_folder_path, file), exist_ok=True)
             ## Images in folder
             for img in os.listdir(os.path.join(folder_path, file)):
-                if img.lower().endswith(image_extension): 
+                if img.lower().endswith(config_items.image_extension): 
                     with Image.open(os.path.join(folder_path, file, img)) as img_file:
                         current_dimension = img_file.size
-                        ratio = min(img_tager_size[0] / current_dimension[0], img_tager_size[1] / current_dimension[1])
+                        ratio = min(config_items.img_tager_size[0] / current_dimension[0], config_items.img_tager_size[1] / current_dimension[1])
                         new_dimension = (int(current_dimension[0] * ratio), int(current_dimension[1] * ratio))
                         resize_image = img_file.resize(size=new_dimension, resample=Image.LANCZOS)
 
                         ## Creating new Canvas
-                        padded_image = Image.new('RGB', size=img_tager_size, color=background_color_padding)
+                        padded_image = Image.new('RGB', size=config_items.img_tager_size, color=config_items.background_color_padding)
 
                         ## calculating center of canvas
-                        x_coordinate = (img_tager_size[0] - new_dimension[0]) // 2 
-                        y_coordinate = (img_tager_size[1] - new_dimension[1]) // 2 
+                        x_coordinate = (config_items.img_tager_size[0] - new_dimension[0]) // 2 
+                        y_coordinate = (config_items.img_tager_size[1] - new_dimension[1]) // 2 
 
                         padded_image.paste(im=resize_image, box=(x_coordinate, y_coordinate))
 
